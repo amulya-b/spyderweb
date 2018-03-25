fa2272ae66af6e5961f624cfc970a764bfa06a12
Amulya Bhattarai <amulsbhat@gmail.com>
2018-03-24 19:10:40 -0700

    API calls on construction of ResultGraphPage

--- src/ResultGraphPage/ResultGraphPage.js
+++ src/ResultGraphPage/ResultGraphPage.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ResultGraphPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            links: []
+        };
+        if (this.props.searchInput !== '') {
+            let input = this.props.searchInput;
+            console.log(input);
+            let searchInput = input.replace(/ /g, "%20");
+            console.log(searchInput);
+            const proxyurl = "https://cors-anywhere.herokuapp.com/"
+            let endpoint = '/w/api.php?action=query&format=json&prop=links&titles=' + searchInput + '&pllimit=max';
+            axios.get(proxyurl + 'https://en.wikipedia.org' + endpoint)
+                .then(response => {
+                    console.log(response.data);
+                    let links = [];
+                    for (let key in response.data.query.pages) {
+                        let element = response.data.query.pages[key];
+                        if (element.title !== input) {
+                            continue;
+                        }
+                        links = element.links;
+                    }
+                    this.setState({
+                        links: links
+                    });
+                    console.log(this.state.links);
+                })
+                .catch(err => {
+                    throw err;
+                });
+        }
+    }
+
     render() {
         console.log(this);
         return (
